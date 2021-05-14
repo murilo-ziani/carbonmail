7840b323ae2e80e8e7a0864b549c8f3297279349
murilo-ziani <murilo.ziani@outlook.com>
2021-05-14 10:35:19 -0300

    criando a conexão com o banco de dados

--- carbonmail/utils/__init__.py
+++ carbonmail/utils/__init__.py
@@ -2,6 +2,17 @@
 # Ele é sempre executado ao importar este pacote
 
 import PySimpleGUI as sg
+import os
+import sys
 
 def inner_element_space(width=100):
     return [sg.Text(' ' * width, font=('Arial', 1)),]
+
+def root_folder():
+    try:
+        main_file = os.path.abspath(sys.modules['__main__'].__file__)
+    except:
+        main_file = sys.executable
+    
+    # irá retornar esse caminho C:\PROJETOS PYTHON\JORNADA\carbonmail\carbonmail\__main__.py
+    return os.path.join(os.path.dirname(main_file),os.pardir)
